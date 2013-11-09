c2e5a030d4073eff4edb581956aa3f399cbf7d6f
Michal Kowalkowski <malcolm.anteros@gmail.com>
2013-11-09 22:43:42 +0800

    update

--- substitutions.js
+++ substitutions.js
@@ -1,21 +1,27 @@
-var words = [
-	{find: /witnesses/gi, replace: 'these dudes I know'},
-	{find: /allegedly/gi, replace: 'kinda probably'},
-	{find: /new study/gi, replace: 'tumbler post'},
-	{find: /rebuild/gi, replace: 'avenge'},
-	{find: /space/gi, replace: 'spaaace'},
-	{find: /google glass/gi, replace: 'virtual boy'},
-	{find: /smartphone/gi, replace: 'Pokédex'},
-	{find: /electric/gi, replace: 'atomic'},
-	{find: /senator/gi, replace: 'elf-lord'},
-	{find: /car/gi, replace: 'cat'},
-	{find: /election/gi, replace: 'eating contest'},
-	{find: /congressional leaders/gi, replace: 'river spirits'},
-	{find: /homeland security/gi, replace: 'homestar runner'},
-	{find: /could not be reached for comment/gi, replace: 'is guilty and everyone knows it'},
-];
+function xkcdSubstitutions(){
+	var words = [
+		{find: /witnesses/gi, replace: 'these dudes I know'},
+		{find: /allegedly/gi, replace: 'kinda probably'},
+		{find: /new study/gi, replace: 'tumbler post'},
+		{find: /rebuild/gi, replace: 'avenge'},
+		{find: /space/gi, replace: 'spaaace'},
+		{find: /google glass/gi, replace: 'virtual boy'},
+		{find: /smartphone/gi, replace: 'Pokédex'},
+		{find: /electric/gi, replace: 'atomic'},
+		{find: /senator/gi, replace: 'elf-lord'},
+		{find: /car/gi, replace: 'cat'},
+		{find: /election/gi, replace: 'eating contest'},
+		{find: /congressional leaders/gi, replace: 'river spirits'},
+		{find: /homeland security/gi, replace: 'homestar runner'},
+		{find: /could not be reached for comment/gi, replace: 'is guilty and everyone knows it'},
+	];
 
-for (var i = 0; i < words.length; i++) {
-	document.body.innerHTML = document.body.innerHTML.replace(words[i].find, words[i].replace);
-};
+	var html = document.body.innerHTML;
+	for (var i = 0; i < words.length; i++) {
+		html = html.replace(words[i].find, words[i].replace);
+	}; 
+	
+	return html;
+}
 
+document.body.innerHTML=xkcdSubstitutions();
