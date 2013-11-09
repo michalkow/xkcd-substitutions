e85aef3284fedf335e40482c049a4c5b49ffc2f8
Michal Kowalkowski <malcolm.anteros@gmail.com>
2013-11-09 22:16:42 +0800

    init

---
+++ substitutions.js
@@ -0,0 +1,20 @@
+var words = [
+	{find: /witnesses/gi, replace: 'these dudes I know'},
+	{find: /allegedly/gi, replace: 'kinda probably'},
+	{find: /new study/gi, replace: 'tumbler post'},
+	{find: /rebuild/gi, replace: 'avenge'},
+	{find: /space/gi, replace: 'spaace'},
+	{find: /google glass/gi, replace: 'virtual boy'},
+	{find: /smartphone/gi, replace: 'Pokedex'},
+	{find: /electric/gi, replace: 'atomic'},
+	{find: /senator/gi, replace: 'elf-lord'},
+	{find: /car/gi, replace: 'cat'},
+	{find: /election/gi, replace: 'eating contest'},
+	{find: /congressional leaders/gi, replace: 'river spirits'},
+	{find: /homeland security/gi, replace: 'homestar runner'},
+	{find: /could not be reached for comment/gi, replace: 'is guilty and everyone knows it'},
+];
+
+for (var i = 0; i < words.length; i++) {
+	document.body.innerHTML = document.body.innerHTML.replace(words[i].find, words[i].replace);
+};
