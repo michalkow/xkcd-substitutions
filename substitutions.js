a2061e9cde9b6bbd7e9909dbcf5fdc95dd7ebd63
Michal Kowalkowski <malcolm.anteros@gmail.com>
2013-11-10 14:14:23 +0800

    better Regex

--- substitutions.js
+++ substitutions.js
@@ -1,26 +1,26 @@
 function xkcdSubstitutions(){
 	var words = [
-		{find: /witnesses/gi, replace: 'these dudes I know'},
-		{find: /allegedly/gi, replace: 'kinda probably'},
-		{find: /new study/gi, replace: 'tumbler post'},
-		{find: /rebuild/gi, replace: 'avenge'},
-		{find: /space/gi, replace: 'spaaace'},
-		{find: /google glass/gi, replace: 'virtual boy'},
-		{find: /smartphone/gi, replace: 'Pokédex'},
-		{find: /electric/gi, replace: 'atomic'},
-		{find: /senator/gi, replace: 'elf-lord'},
-		{find: /car/gi, replace: 'cat'},
-		{find: /election/gi, replace: 'eating contest'},
-		{find: /congressional leaders/gi, replace: 'river spirits'},
-		{find: /homeland security/gi, replace: 'homestar runner'},
-		{find: /could not be reached for comment/gi, replace: 'is guilty and everyone knows it'},
+		{find: new RegExp('witnesses\s', 'gi'), replace: 'these dudes I know '},
+		{find: new RegExp('allegedly\s', 'gi'), replace: 'kinda probably '},
+		{find: new RegExp('new study\s', 'gi'), replace: 'tumbler post '},
+		{find: new RegExp('rebuild\s', 'gi'), replace: 'avenge '},
+		{find: new RegExp('space\s', 'gi'), replace: 'spaaace '},
+		{find: new RegExp('google glass\s', 'gi'), replace: 'virtual boy '},
+		{find: new RegExp('smartphone\s', 'gi'), replace: 'Pokédex '},
+		{find: new RegExp('electric\s', 'gi'), replace: 'atomic '},
+		{find: new RegExp('senator\s', 'gi'), replace: 'elf-lord '},
+		{find: new RegExp('car\s', 'gi'), replace: 'cat '},
+		{find: new RegExp('election\s', 'gi'), replace: 'eating contest '},
+		{find: new RegExp('congressional leaders\s', 'gi'), replace: 'river spirits '},
+		{find: new RegExp('homeland security\s', 'gi'), replace: 'homestar runner '},
+		{find: new RegExp('could not be reached for comment\s', 'gi'), replace: 'is guilty and everyone knows it '},
 	];
 
 	var html = document.body.innerHTML;
 	for (var i = 0; i < words.length; i++) {
 		html = html.replace(words[i].find, words[i].replace);
 	}; 
-	
+
 	return html;
 }
 
